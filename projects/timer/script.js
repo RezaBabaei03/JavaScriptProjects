5304f76a8f8df4149f3f805e6937a41b23add2d4
RezaBabaei03 <rezababaei2003@gmail.com>
2024-07-14 15:42:56 +0330

    First commit of timer project

---
+++ projects/timer/script.js
@@ -0,0 +1,15 @@
+
+// Timed showing html contents
+
+const timer = function (_time) {
+  if (_time > 0) {
+      document.querySelector("#head3").textContent = _time    ;
+      window.setTimeout(function () {timer(_time - 1)}, 1000);
+  } else {
+      document.querySelector("#head3").textContent = 0;
+      window.setTimeout(function () {alert("Time ended.")}, 10);
+  }
+}
+
+const _time = Number(prompt("Seconds: "));
+timer(_time);
